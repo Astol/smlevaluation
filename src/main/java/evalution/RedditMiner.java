943a92c6f42635c51b84bf39bf3efe777d079abe
Fredrik Jonsén <frejo105@student.liu.se>
2017-05-09 16:05:51 +0200

    Created base for evaluation application

--- src/main/java/evalution/RedditMiner.java
+++ src/main/java/evalution/RedditMiner.java
@@ -1,10 +1,33 @@
 package evalution;
 
 import net.dean.jraw.RedditClient;
+import net.dean.jraw.http.UserAgent;
+import net.dean.jraw.http.oauth.Credentials;
+import net.dean.jraw.http.oauth.OAuthData;
+import net.dean.jraw.http.oauth.OAuthException;
 import org.cfg4j.provider.ConfigurationProvider;
 
 public class RedditMiner {
-    public RedditMiner() {
-        ConfigurationProvider config = Configuration.getInstance().getConfig();
+    public RedditClient getClient() {
+        return client;
     }
+
+    private RedditClient client;
+
+    public RedditMiner() throws OAuthException {
+        ConfigurationProvider config = Configuration.getInstance().getConfig();
+        String username = config.getProperty("REDDIT.USERNAME", String.class);
+        String password = config.getProperty("REDDIT.PASSWORD", String.class);
+        String appId = config.getProperty("REDDIT.APPID", String.class);
+        String appSecret = config.getProperty("REDDIT.APPSECRET", String.class);
+        Credentials credentials = Credentials.script(username, password, appId, appSecret);
+
+        UserAgent userAgent = UserAgent.of("desktop", appId, "v0.1", "SocialMediaLibrary");
+
+        client = new RedditClient(userAgent);
+        OAuthData data = client.getOAuthHelper().easyAuth(credentials);
+        client.authenticate(data);
+    }
+
+
 }
--- src/main/java/evalution/SMLEvaluation.java
+++ src/main/java/evalution/SMLEvaluation.java
@@ -1,8 +1,11 @@
 package evalution;
 
+import net.dean.jraw.http.oauth.OAuthException;
+
 public class SMLEvaluation {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws OAuthException {
+        RedditMiner miner = new RedditMiner();
+        System.out.println(miner.getClient().me());
     }
 }
